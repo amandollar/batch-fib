3020255292ebe240cd78f300fe135aab74889348
Aman Sharma <135960284+amandollar@users.noreply.github.com>
2025-06-02 09:55:35 +0530

    Day 2 and Day 3 glimpses

---
+++ Day2/day.js
@@ -0,0 +1,40 @@
+
+// const num = 23;
+// num = 23;
+// console.log(num);
+
+
+
+// const Student = {
+//     name:"Aman",
+//     roll:66,
+//     regNo:234343
+// }
+
+// Student["name"] = "Anonymous";
+
+
+
+//Data types
+
+
+//Primitive
+
+// let a = "aman";
+// let b = false;
+// console.log(typeof b);
+
+
+//Non prmitive
+
+// let nums = [2,3,4,9];
+// console.log(nums);
+
+// const Student = {
+//     name:"Aman",
+//     roll:66,
+//     regNo:234343
+// }
+
+// Student["name"] = "Anonymous";
+
